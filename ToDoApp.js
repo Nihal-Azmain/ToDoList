ee8830a6038d08919c94c3df01805c303da73591
Nihal-Azmain <n.azmain01@gmail.com>
2023-01-20 10:57:07 +0600

    0.1.0

--- ToDoApp.js
+++ ToDoApp.js
@@ -1,3 +1,5 @@
+// ***ADD WORK TO THE LIST****
+
 let add=document.getElementById('add');
 
 add.addEventListener('click',function(event){
@@ -8,20 +10,40 @@ add.addEventListener('click',function(event){
     document.getElementById('inputBox').value="";
 });
 
-
-
 function addToList(text)
 {
     let textNode=document.createTextNode(text);
 
+    let checkBox=document.createElement('input');
+    checkBox.type="checkbox";
+    checkBox.name=text;
+    
     let span=document.createElement('span');
     span.className="close";
-    span.appendChild(textNode);
+    span.appendChild(document.createTextNode('x'));
     span.style.fontSize="20px";
 
     let newItem=document.createElement('li');
     newItem.style.paddingBottom="10px";
+    newItem.appendChild(checkBox);
+    newItem.appendChild(textNode);
     newItem.appendChild(span);
 
     document.getElementById('list').appendChild(newItem);
+    removeItem(newItem);
 }
+
+// ***REMOVE THE LIST AFTER PRESSING THE CLOSE BUTTON***
+
+function removeItem(item)
+{
+
+    let closebtn =item.lastElementChild;
+    // console.log(closebtn);
+
+    closebtn.addEventListener('click', function(){
+    event.preventDefault();
+    item.parentElement.removeChild(item);
+    });
+}
+
